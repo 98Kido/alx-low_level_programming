4bf3aad0113a8e4a138ee7588313d2badff9de07
Vuyiswa Sibisi <vpalesa23@gmail.com>
2023-03-23 18:39:15 -0700

    funtions nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of two numbers
+ * @a: int number
+ * @b: int number
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Sec
