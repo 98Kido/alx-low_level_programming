539715e6d59cd2404a7da32b147aaaa80be00548
98Kido <125591706+98Kido@users.noreply.github.com>
2023-08-16 21:43:24 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,36 +1,27 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - A simple struct that consists of all variables.
+ * @name: Holds name of dog in char array
+ * @age: Holds number, including any decimals
+ * @owner: Holds name of owner in char array
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
-	/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-
-	/**
-	 * dog_t - typedef for struct dog
-	 */
-	typedef struct dog dog_t;
-
-
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
-
-
-	#endif
-
+#endif
